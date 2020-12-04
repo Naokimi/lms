754820f4e8dfb6053b3ef6208645ef43374ff131
Paulo DAlberti <phdalberti@gmail.com>
2020-12-04 16:22:41 +0900

    fixed user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@
 #
 #  id           :bigint           not null, primary key
 #  admin        :boolean
+#  email        :string
 #  first_name   :string
 #  last_name    :string
 #  subscription :integer
+#  uid          :string
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #
--- db/schema.rb
+++ db/schema.rb
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 2020_12_01_134808) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "uid"
+    t.string "email"
     t.string "first_name"
     t.string "last_name"
     t.boolean "admin"
