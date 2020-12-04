20227b9f65f3830fc3dd5c1c1415ae61d371af3f
Paulo DAlberti <phdalberti@gmail.com>
2020-12-05 00:51:47 +0900

    login admin

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::UsersController < ActionController::API
+  before_action :set_user, only: %i[login admin]
+
   def create
     @user = User.new(user_params)
 
@@ -6,16 +8,22 @@ class Api::V1::UsersController < ActionController::API
       @user.save!
       token = TokenRequester.new.call
       @user.update(token: token)
-      # render json: user, status: :created
       render :show, status: :created
     rescue StandardError => e
       render json: e, status: 500
     end
   end
 
-  # login
+  def login
+    token = TokenRequester.new.call
+    @user.update(token: token)
+    render :show
+  end
 
-  # admin
+  def admin
+    @user.update(admin: true)
+    render json: { message: "User #{@user.id} upgraded to admin" }
+  end
 
   # regular subscription
 
