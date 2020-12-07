9bfa9b3f008807e6f3eeb3fe995581209f939dcc
Paulo DAlberti <phdalberti@gmail.com>
2020-12-07 11:02:26 +0900

    secure controller spec

--- app/controllers/api/v1/secure_controller.rb
+++ app/controllers/api/v1/secure_controller.rb
@@ -28,7 +28,7 @@ class Api::V1::SecureController < ActionController::API
   end
 
   def set_raven_context
-    Raven.user_context(id: current_user.id)
+    Raven.user_context(id: current_user&.id)
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
 end
--- spec/controllers/api/v1/secure_controller_spec.rb
+++ spec/controllers/api/v1/secure_controller_spec.rb
@@ -14,18 +14,33 @@ RSpec.describe Api::V1::SecureController, type: :controller do
 
     before do
       request.headers['Authorization'] = "Bearer #{user.token}"
+      allow(AuthorizationService).to receive(:new).and_return(authorization_service)
     end
 
     context 'success' do
       before do
-        allow(AuthorizationService).to receive(:new).and_return(authorization_service)
         allow(authorization_service).to receive(:authenticate_request!)
       end
 
       it 'authorizes action' do
         get :index, format: :json
+        expect(response).to have_http_status(204)
         expect(AuthorizationService).to have_received(:new)
         expect(authorization_service).to have_received(:authenticate_request!)
+      end
+    end
+
+    context 'failure' do
+      before do
+        allow(authorization_service).to receive(:authenticate_request!).and_raise(JWT::DecodeError)
+      end
+
+      it 'removes token and renders unauthorized' do
+        get :index, format: :json
+        expect(response).to have_http_status(:unauthorized)
+        expect(AuthorizationService).to have_received(:new)
+        expect(authorization_service).to have_received(:authenticate_request!)
+        expect(User.find(user.id).token).to eq(nil)
       end
     end
   end
